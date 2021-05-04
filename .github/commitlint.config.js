13ba1991db896e61f444e3cfb8c5b9b2e91cbfbd
hanchchch <cndghks15@gmail.com>
2021-05-05 01:43:49 +0900

    doc: commit lint

---
+++ .github/commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	rules: {
+		'body-leading-blank': [1, 'always'],
+		'footer-leading-blank': [1, 'always'],
+		'header-max-length': [2, 'always', 72],
+		'scope-case': [2, 'always', 'lower-case'],
+		'subject-empty': [2, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+		'type-case': [2, 'always', 'lower-case'],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'refactor',
+				'test',
+				'revert',
+				'doc',
+				'style',
+				'misc'
+			]
+		],
+	}
+};
