538b785b68554c8f3193c3bd453b675239f6c269
hanchchch <cndghks15@gmail.com>
2021-05-06 15:39:40 +0900

    feat: account app, User and Profile model

--- devathon/settings/base.py
+++ devathon/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [
@@ -82,3 +83,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
