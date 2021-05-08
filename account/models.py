8acf96778beaaa82d3c4fa0e19c7f53fc8534cd2
hanchchch <cndghks15@gmail.com>
2021-05-08 17:46:39 +0900

    feat: accout models Position, Team, Profile

--- account/models.py
+++ account/models.py
@@ -4,5 +4,20 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Position(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    team = models.ManyToManyField(
+        Team, on_delete=models.SET_NULL, related_name="user_set", null=True
+    )
+    url = models.URLField(blank=True)
+    position = models.ManyToManyField(
+        Position, on_delete=models.SET_NULL, related_name="user_set", null=True
+    )
