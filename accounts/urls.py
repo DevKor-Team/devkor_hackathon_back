dcba5c43a9e373fce0d1f21831ed04ae1c6c0991
hanchchch <cndghks15@gmail.com>
2021-05-16 14:33:48 +0900

    feat: ProfileViewSet

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 router = routers.DefaultRouter()
+router.register("profile/", views.ProfileViewSet)
 
 urlpatterns = [
-    path("me", views.MeView.as_view()),
+    path("me/", views.MeView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.views import APIView
+from rest_framework import mixins
 
 from accounts.models import User
-from accounts.serializers import UserSerializer
+from accounts.serializers import UserSerializer, ProfileSerializer
+
+
+class ProfileViewSet(GenericViewSet, mixins.UpdateModelMixin):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]  # TODO IsMyProfile
 
 
 class MeView(APIView):
