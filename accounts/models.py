22e5b9af2e7bfa414341fd4287e97eaf67bc7fed
hanchchch <cndghks15@gmail.com>
2021-05-31 17:01:49 +0900

    fix: Team token create and verify

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,8 @@ from hashlib import sha256
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.conf import settings
 
-from devathon.settings.base import SECRET_KEY
 from accounts.services import no_padding_b64encode, no_padding_b64decode
 
 
@@ -22,17 +22,20 @@ class Team(models.Model):
     users = models.ManyToManyField(User, related_name="teams")
 
     def sign_token(self, timestamp: bytes):
-        return sha256(self.name.encode() + timestamp + SECRET_KEY.encode()).digest()
+        return sha256(
+            self.name.encode() + timestamp + settings.SECRET_KEY.encode()
+        ).digest()
 
     def create_token(self):
-        expire = (timezone.now() + timedelta(hours=1)).timestamp()
+        expire = (timezone.now() + timedelta(hours=6)).timestamp()
         expire_bytes = int(expire).to_bytes(5, byteorder="little")
         sign = self.sign_token(expire_bytes)
-        self.token = no_padding_b64encode(expire_bytes + sign)
+        self.token = no_padding_b64encode(expire_bytes + sign).decode()
+        self.save()
 
     def verify_token(self, token):
         try:
-            token_decoded = no_padding_b64decode(token)
+            token_decoded = no_padding_b64decode(token.encode())
             expire_bytes = token_decoded[:5]
             sign = token_decoded[5:]
             expire = int.from_bytes(expire_bytes, byteorder="little")
