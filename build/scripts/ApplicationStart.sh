4267a8e5e70e53bf0ba256603425191952fce5af
hanchchch <cndghks15@gmail.com>
2021-06-26 21:38:39 +0900

    chore: docker starts with env

--- build/scripts/ApplicationStart.sh
+++ build/scripts/ApplicationStart.sh
@@ -1,1 +1,8 @@
-docker run -d --rm --name hackathon-back -p 8000:8000 -v /home/ec2-user/hackathon/back:/app hackathon-back:latest
+docker run -dit \
+-e SECRET_KEY=$SECRET_KEY \
+-e GOOGLE_OAUTH_CLIENT_ID=$GOOGLE_OAUTH_CLIENT_ID \
+-e GOOGLE_OAUTH_SECRET=$GOOGLE_OAUTH_SECRET \
+--rm --name hackathon-back \
+-p 8000:8000 \
+-v /home/ec2-user/hackathon/back:/app \
+hackathon-back:latest
