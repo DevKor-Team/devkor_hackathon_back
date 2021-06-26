1d632ef9413f286ab1e822fb76f9626b5f838606
hanchchch <cndghks15@gmail.com>
2021-06-26 20:50:28 +0900

    chore: prod temp database setting, wsgi setting

--- devathon/settings/produnction.py
+++ devathon/settings/produnction.py
@@ -7,15 +7,24 @@ ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ORIGIN_WHITELIST = []
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USERNAME"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USERNAME"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+LOGIN_REDIRECT_URL = "/"
 
 MIDDLEWARE = [*MIDDLEWARE, "django.middleware.csrf.CsrfViewMiddleware"]
