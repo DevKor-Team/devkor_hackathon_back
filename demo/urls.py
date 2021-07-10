fabc0d60223f313c9191d41e34e8d02ce60306bc
seoljiwon <suljiwon@naver.com>
2021-07-11 07:55:52 +0900

    feat: demo routing

--- demo/urls.py
+++ demo/urls.py
@@ -1,5 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from django.urls import path, include
+from . import views
+
+router = routers.DefaultRouter()
+router.register("demo", views.DemoViewSet)
 
 urlpatterns = [
 
