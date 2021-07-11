ea7368b4a8fd15ef6cb3a7aacf2d9451d0c5d7c8
seoljiwon <suljiwon@naver.com>
2021-07-11 11:58:50 +0900

    feat: tag serializer

--- demo/serializers.py
+++ demo/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
+from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
 from .models import Demo
-from accounts.serializers import TeamSerializer
 
 
-class DemoSerializer(serializers.ModelSerializer):
-    team = TeamSerializer(read_only=True)
+class DemoSerializer(TaggitSerializer, serializers.ModelSerializer):
+    tech_stacks = TagListSerializerField()
+    tags = TagListSerializerField()
 
     class Meta:
         model = Demo
-        fields = ["id", "team", "title", "thumbnail", "desc", "created_at", "updated_at"]
+        fields = ["id", "team", "title", "thumbnail", "desc", "created_at", "tech_stacks", "tags"]
