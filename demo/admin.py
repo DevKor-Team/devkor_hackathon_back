5d58b8c2cddb45db74ee1bc0dde5324c06a127cc
seoljiwon <suljiwon@naver.com>
2021-07-11 12:00:09 +0900

    feat: admin register team, tag

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Demo
+from .models import Demo, Tag, TechStackTag
 
 
 admin.site.register(Demo)
+admin.site.register(Tag)
+admin.site.register(TechStackTag)
