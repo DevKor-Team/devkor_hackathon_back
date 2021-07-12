d60cfa9c2d4fdfc4921c3a6ad3a5157b8af384d0
seoljiwon <suljiwon@naver.com>
2021-07-12 10:36:50 +0900

    feat: tags, tech_stacks multiple filtering

--- demo/filters.py
+++ demo/filters.py
@@ -23,21 +23,22 @@ class DemoFilter(filters.FilterSet):
 
     @staticmethod
     def get_tag(queryset, name, value):
+        tags = value.split(",")
         return queryset.filter(
-            tags__name=value
+            tags__name__in=tags
         ).distinct()
 
     @staticmethod
     def get_tech_stack(queryset, name, value):
+        tech_stacks = value.split(",")
         return queryset.filter(
-            tech_stacks__name=value
+            tech_stacks__name__in=tech_stacks
         ).distinct()
 
     class Meta:
         model = Demo
         fields = {
             "created_at": [
-                "exact"
             ],
             "tags": [
             ],
