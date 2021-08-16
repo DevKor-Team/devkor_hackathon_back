4c15b81d111640fe94c19c0c2576f391794ecc00
seol <suljiwon@naver.com>
2021-08-17 00:48:25 +0900

    feat: add emoji url

--- demo/urls.py
+++ demo/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register("demo", views.DemoViewSet)
 router.register("comments", views.CommentViewSet)
+router.register("emoji", views.EmojiViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
