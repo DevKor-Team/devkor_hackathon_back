37b912e4e76784c6f09f8dd1bb298a07b2dfa475
hanchchch <cndghks15@gmail.com>
2021-08-18 21:46:27 +0900

    fix: start script docker run with env

--- build/scripts/ApplicationStart.sh
+++ build/scripts/ApplicationStart.sh
@@ -3,6 +3,10 @@ docker run -dit \
 -e SECRET_KEY=123123123 \
 -e GOOGLE_OAUTH_CLIENT_ID=$GOOGLE_OAUTH_CLIENT_ID \
 -e GOOGLE_OAUTH_SECRET=$GOOGLE_OAUTH_SECRET \
+-e DB_NAME=$DB_NAME \
+-e DB_USERNAME=$DB_USERNAME \
+-e DB_PASSWORD=$DB_PASSWORD \
+-e DB_HOST=$DB_HOST \
 --rm --name hackathon-back \
 -p 8000:8000 \
 -v /home/ec2-user/hackathon/back:/app \
