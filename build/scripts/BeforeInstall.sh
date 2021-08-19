2edeb25f817600302875e3fe32a12db267fa3087
hanchchch <cndghks15@gmail.com>
2021-08-19 23:01:34 +0900

    build: BeforeInstall

---
+++ build/scripts/BeforeInstall.sh
@@ -0,0 +1,1 @@
+aws ecr get-login-password --region ap-northeast-2 | sudo docker login --username AWS --password-stdin 792939917746.dkr.ecr.ap-northeast-2.amazonaws.com
--- build/scripts/Install.sh
+++ build/scripts/Install.sh
@@ -1,4 +1,3 @@
-aws ecr get-login-password --region ap-northeast-2 | sudo docker login --username AWS --password-stdin 792939917746.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull 792939917746.dkr.ecr.ap-northeast-2.amazonaws.com/devkor-hackathon-back:latest
 docker pull 792939917746.dkr.ecr.ap-northeast-2.amazonaws.com/devkor-hackathon-front:latest
 docker tag 792939917746.dkr.ecr.ap-northeast-2.amazonaws.com/devkor-hackathon-back:latest devkor-hackathon-back:latest
