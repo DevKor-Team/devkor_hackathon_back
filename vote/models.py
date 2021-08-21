1890c1b1255dccc6754fa77b926c01fe23c2c5ad
hanchchch <cndghks15@gmail.com>
2021-08-22 00:57:00 +0900

    feat: VoteSchedule, migration file changed

--- vote/models.py
+++ vote/models.py
@@ -1,13 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
 from accounts.models import Team
 from demo.models import Demo
 
 
+class VoteSchedule(models.Model):
+    start_at = models.DateTimeField(default=timezone.now)
+    end_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{} - {}".format(self.start_at, self.end_at)
+
+    @staticmethod
+    def currently():
+        schedule = VoteSchedule.objects.filter(
+            start_at__lte=timezone.now(),
+            end_at__gte=timezone.now(),
+        )
+        if schedule.count() == 1:
+            return schedule[0]
+        else:
+            return None
+
+
 class Vote(models.Model):
     team = models.ForeignKey(Team, related_name="votes", on_delete=models.CASCADE)
     demo = models.ForeignKey(Demo, related_name="votes", on_delete=models.CASCADE)
-    priority = models.IntegerField(default=0)
+    schedule = models.ForeignKey(
+        VoteSchedule, related_name="votes", on_delete=models.CASCADE
+    )
+    priority = models.SmallIntegerField(default=0)
 
     def __str__(self):
         return "{} - {}".format(self.user, self.demo)
