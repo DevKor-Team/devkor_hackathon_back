110a480e3e9db043be11748b4359b1807d2bb815
hanchchch <cndghks15@gmail.com>
2021-08-22 12:44:21 +0900

    fix: page size 100

--- devathon/settings/base.py
+++ devathon/settings/base.py
@@ -111,7 +111,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 12,
+    "PAGE_SIZE": 100,
 }
 
 LANGUAGE_CODE = "en-us"
