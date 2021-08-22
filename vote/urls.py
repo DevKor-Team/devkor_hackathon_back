96f6afd2d0ebe1d933d21ff1b2c008545134b898
hanchchch <cndghks15@gmail.com>
2021-08-22 12:55:04 +0900

    feat: VoteResultAPIView

--- vote/urls.py
+++ vote/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("vote", views.VoteAPIView.as_view()),
     path("votable", views.VotableAPIView.as_view()),
+    path("result", views.VoteResultAPIView.as_view()),
 ]
